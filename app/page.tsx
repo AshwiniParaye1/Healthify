cc93dacb1b60f247802cf9f19424895fc677e486
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-10-07 19:01:10 +0530

    added theme

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold text-white underline">Home</h1>
-      <Button className="bg-red-500">Click</Button>
+      <Button>Click</Button>
     </div>
   );
 }
