d17abcccd5e0327d4f7d36bd847a04f16ac780f2
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-10-07 19:32:26 +0530

    created basuc styling of home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import PatientForm from "@/components/forms/PatientForm";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -13,8 +15,27 @@ export default function Home() {
             height={1000}
             className="mb-12 h-10 w-fit"
           />
+
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justigy-items-end text-dark-600 xl:text-left">
+              © 2024 Healthify
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
         </div>
       </section>
+
+      <Image
+        src="/asse ts/images/onboarding-img.png"
+        alt="patient"
+        width={1000}
+        height={1000}
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
