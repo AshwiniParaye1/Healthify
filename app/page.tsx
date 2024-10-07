54cb8d6014bbc7290ed33859431d88791435fa1f
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-10-07 18:32:24 +0530

    fixed import error

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold text-white underline">Home</h1>
+      <Button className="bg-red-500">Click</Button>
     </div>
   );
 }
