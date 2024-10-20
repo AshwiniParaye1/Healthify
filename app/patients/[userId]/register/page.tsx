ed146d66575cc385b0ee2b0af25cdd7323730682
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-10-20 12:00:00 +0530

    created register form

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -1,20 +1,44 @@
+import RegisterForm from "@/components/forms/RegisterForm";
+import { getUser } from "@/lib/actions/patient.actions";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-const Register = () => {
+const Register = async ({ params: { userId } }: SearchParamProps) => {
+  const user = await getUser(userId);
+
   return (
     <div className="flex h-screen max-h-screen">
-      <section className="remove-scrollbar container">
-        <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
           <Image
             src="/assets/icons/logo.svg"
-            height={1000}
+            alt="patient"
             width={1000}
-            alt="healthify"
+            height={1000}
             className="mb-12 h-10 w-fit"
           />
+
+          <RegisterForm user={user} />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justigy-items-end text-dark-600 xl:text-left">
+              © 2024 Healthify
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
         </div>
       </section>
+
+      <Image
+        src="/assets/images/register-img.png"
+        alt="patient"
+        width={1000}
+        height={1000}
+        className="side-img max-w-[390px]"
+      />
     </div>
   );
 };
