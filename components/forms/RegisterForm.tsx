84b291a03f70a4d24dfc4171c15065ead7cf060d
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-11-11 14:06:20 +0530

    ignore ts error

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -9,7 +9,7 @@ import SubmitButton from "../SubmitButton";
 import { useState } from "react";
 import { PatientFormValidation, UserFormValidation } from "@/lib/validation";
 import { useRouter } from "next/navigation";
-import { createUser } from "@/lib/actions/patient.actions";
+import { createUser, registerPatient } from "@/lib/actions/patient.actions";
 import { FormFieldType } from "./PatientForm";
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
 import {
@@ -65,6 +65,7 @@ const RegisterForm = ({ user }: { user: User }) => {
         identificationDocument: formData
       };
 
+      // @ts-ignore
       const patient = await registerPatient(patientData);
 
       if (patient) {
